89815c6176f21dd2f2dea418e7dae3651c7ee9e7
HolyPineApple <85448243+HolyPineApple@users.noreply.github.com>
2022-01-09 10:30:19 +0300

    Initial commit

---
+++ Assets/Scripts/InputFieldScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputFieldScript : MonoBehaviour
+{
+    public GameObject _panelInput;
+    public InputField _inputField;
+    public Text _changeText;
+
+    public void SaveInput()
+    {
+        if(_inputField.text=="")
+        {
+            Debug.Log("ERROR");
+        }
+        else
+        {
+            Debug.Log("GREAT");
+            _changeText.text = _inputField.text;
+            _panelInput.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/TransportfProperties.cs
@@ -0,0 +1,44 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TransportfProperties: MonoBehaviour
+{
+    [Header("Масса Транспорта")]
+    public float _weight;
+    [Header("Вместимость Транспорта")]
+    public int _capacity;
+    [Header("Макс. скорость Транспорта")]
+    public float _maxSpeed;
+    [Header("Название Транспорта")]
+    public string _titleTransport;
+    [Header("Уникаольное свойство Транспорта")]
+    public string _uniqueProperty;
+    [Header("Название уникаольного свойство Транспорта")]
+    public string _uniquePropertyTitle;
+    [Header("Масса ТекстUI")]
+    public Text _weightText;
+    [Header("Вместимость ТекстUI")]
+    public Text _capacityText;
+    [Header("Макс. скорость ТекстUI")]
+    public Text _maxSpeedText;
+    [Header("Название транспорта ТекстUI")]
+    public Text _titleTransportText;
+    [Header("Уникальное свойство ТекстUI")]
+    public Text _uniquePropertyText;
+    [Header("Название уникального свойства ТекстUI")]
+    public Text _uniquePropertyTitleText;
+        
+
+    private void Start()
+    {
+        _weightText.text = "" + _weight;
+        _capacityText.text = "" + _capacity;
+        _maxSpeedText.text = "" + _maxSpeed;
+        _titleTransportText.text = _titleTransport;
+        _uniquePropertyText.text = "" +_uniqueProperty;
+        _uniquePropertyTitleText.text = _uniquePropertyTitle+"-";
+    }
+
+   
+}
